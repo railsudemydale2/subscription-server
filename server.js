7812e1dd2e4899547d011c0f9fd2449a9c90076b
dale <stewartdale45@yahoo.com>
2021-11-19 18:56:54 -0500

    Implemented DATABASE CONNECTION

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { readdirSync } = require('fs');
+require('dotenv').config();
+
+// import routes
+// const authRoutes = require('./routes/auth);
+
+// app
+const app = express();
+
+// db
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB CONNECTION WAS SUCCESSFUL'))
+  .catch((err) => console.log(`DB CONNECTION ERR ${err}`));
+
+// middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json({ limit: '2mb' }));
+app.use(cors());
+
+// routes middlewares
+// app.use('/api', authRoutes);
+readdirSync('./routes').map((r) => app.use('/api', require('./routes/' + r)));
+
+// PORT
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
