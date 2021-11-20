3518a298361479a0b3251d30e58a6795e6ae182a
dale <stewartdale45@yahoo.com>
2021-11-19 21:09:50 -0500

    Implemented the functionality to show validation and error messages

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,27 @@
-export const register = (req, res) => {
+export const register = async (req, res) => {
   //   res.send('Hey, you reached node server');
-  res.json({
-    data:
-      'Success, you used Vulcan Logic to reach node server ====> this is from controllers!!!',
-  });
+  // res.json({
+  //   data:
+  //     'Success, you used Vulcan Logic to reach node server ====> this is from controllers!!!',
+  // });
+  try {
+    // validation
+    // console.log(req.body);
+    const { name, password } = req.body;
+    if (!name) {
+      return res.json({
+        error: 'Name is required!',
+      });
+    }
+    if (!password || password.length < 6) {
+      return res.json({
+        error: 'Password is required and should be 6 characters long',
+      });
+    }
+    res.json({
+      data: 'This is /register endpoint',
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
