75535a1f5124834a8a03ae528ae4cdeb3dc3d963
dale <stewartdale45@yahoo.com>
2021-11-19 19:31:48 -0500

    Implemented Middlewares and routes

--- server.js
+++ server.js
@@ -1,38 +1,32 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const { readdirSync } = require('fs');
-require('dotenv').config();
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import { readdirSync } from "fs";
 
-// import routes
-// const authRoutes = require('./routes/auth);
+const morgan = require("morgan");
+require("dotenv").config();
 
-// app
 const app = express();
 
 // db
 mongoose
-  .connect(process.env.DATABASE, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log('DB CONNECTION WAS SUCCESSFUL'))
-  .catch((err) => console.log(`DB CONNECTION ERR ${err}`));
+  .connect(process.env.DATABASE)
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log("DB Connection Error ", err));
 
 // middlewares
-app.use(morgan('dev'));
-app.use(bodyParser.json({ limit: '2mb' }));
-app.use(cors());
+app.use(express.json({ limit: "5mb" }));
+app.use(
+  cors({
+    origin: [process.env.CLIENT_URL],
+  })
+);
 
-// routes middlewares
-// app.use('/api', authRoutes);
-readdirSync('./routes').map((r) => app.use('/api', require('./routes/' + r)));
+// autoload routes
+app.get("/api/register", (req, res) => {
+  res.send("Hey you reached node server");
+});
 
-// PORT
-const port = process.env.PORT || 8000;
-
+// listen
+const port = process.env.PORT || 8002;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
