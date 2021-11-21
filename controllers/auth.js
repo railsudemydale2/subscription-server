87a447089a5d1deebef20b1e337a30085aab70d3
dale <stewartdale45@yahoo.com>
2021-11-20 20:49:19 -0500

    Implemented JWT and user response on login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 import User from '../models/user';
 import { hashPassword, comparePassword } from '../helpers/auth';
+import jwt from 'jsonwebtoken';
 
 export const register = async (req, res) => {
   //   res.send('Hey, you reached node server');
@@ -44,6 +45,37 @@ export const register = async (req, res) => {
     } catch (err) {
       console.log(err);
     }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    // console.log(req.body);
+    // check email
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.json({
+        error: 'No user found',
+      });
+    }
+    // check password
+    const match = await comparePassword(req.body.password, user.password);
+    if (!match) {
+      return res.json({ error: 'Invalid password, try logging in again' });
+    }
+    // create signed token
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: '7d',
+    });
+
+    const { password, ...rest } = user._doc;
+
+    res.json({
+      token,
+      user: rest,
+    });
   } catch (err) {
     console.log(err);
   }
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import { register } from '../controllers/auth';
+import { register, login } from '../controllers/auth';
 const router = express.Router();
 
 router.post('/register', register);
+router.post('/login', login);
 
 module.exports = router;
