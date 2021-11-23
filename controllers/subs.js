a270ac35782f9458b7267a1deeff0c610acee493
dale <stewartdale45@yahoo.com>
2021-11-23 08:59:19 -0500

    Implemented the functionality to show User Subscriptions

--- controllers/subs.js
+++ controllers/subs.js
@@ -55,3 +55,20 @@ export const subscriptionStatus = async (req, res) => {
     console.log(err);
   }
 };
+
+export const subscriptions = async (req, res) => {
+  try {
+
+    const user = await User.findById(req.user._id);
+    
+    const subscriptions = await stripe.subscriptions.list({
+      customer: user.stripe_customer_id,
+      status: 'all',
+      expand: ['data.default_payment_method'],
+    });
+
+    res.json(subscriptions);
+  } catch (err) {
+    console.log(err);
+  }
+};
